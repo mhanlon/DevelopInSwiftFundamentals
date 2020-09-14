1537af37926bc40fd1596a1b252131ad76113c47
Matt Hanlon <mhanlon@gmail.com>
2020-09-14 08:17:18 +0100

    My first lesson, done

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,13 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Hello, world!")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
+print("Here")
+print("we")
+print("go")
 
 /*:
 
