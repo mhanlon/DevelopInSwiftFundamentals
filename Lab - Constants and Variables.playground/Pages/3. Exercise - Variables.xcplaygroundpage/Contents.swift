b8b9435f19ca9bbbd88e4c77e660e6a888e3a1d1
Giovanna Busconi <giovanna.busconi@gmail.com>
2020-11-17 00:28:22 +0100

    exercises

--- Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -3,16 +3,18 @@
  
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
-
+var schooling = 18
+print(schooling)
 
 /*:
  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
  */
-
+schooling = 19
+print(schooling)
 
 /*:
  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
  */
-
+print("A variable could change her value")
 
 //: [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
--- Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,21 +3,26 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-
+var firstDecimal = 2.0
+var secondDecimal = 10.0
 
 /*:
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var trueOrFalse = true
+// firstDecimal = trueOrFalse
+print("the cariables are of different type")
 
 /*:
  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
-
+var name = "Giovanna"
+//firstDecimal = name
+print("the cariables are of different type")
 /*:
  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
-
+var wholeNumber = 100
+//firstDecimal = wholeNumber
+print("the cariables are of different type")
 //: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
