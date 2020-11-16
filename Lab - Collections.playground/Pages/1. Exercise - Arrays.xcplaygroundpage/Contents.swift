fe3f28fb9bc2e131472637b6c4bf1220fc5f7d02
Giovanna Busconi <giovanna.busconi@gmail.com>
2020-11-16 21:37:32 +0100

    Exercises
    I did four exercises ...

--- Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
+++ Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -3,31 +3,36 @@
  
  Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
  */
-
-
+var registrationList: [String] = []
 /*:
  Your friend Sara is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection.
  */
-
+registrationList.append("Sara")
+print(registrationList)
 
 /*:
  Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
  */
-
+registrationList += ["Mario", "Luca","Giorgia","Lucia"]
+print(registrationList)
 
 /*:
  Use the `insert(_:at:)` method to add `Charlie` into the array as the second element. Print the contents of the collection.
  */
-
+registrationList.insert("Charlie", at: 1)
+print(registrationList)
 
 /*:
  Someone had a conflict and decided to transfer her registration to someone else. Use array subscripting to change the sixth element to `Rebecca`. Print the contents of the collection.
  */
+registrationList[5] = "Rebecca"
+print(registrationList)
 
 
 /*:
  Call `removeLast()` on `registrationList`. If done correctly, this should remove `Rebecca` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`.
  */
-
+let deleteItem = registrationList.removeLast()
+print(deleteItem)
 
 //: page 1 of 4  |  [Next: App Exercise - Activity Challenge](@next)
--- Lab - Guard.playground/Pages/1. Exercise - Guard Statements.xcplaygroundpage/Contents.swift
+++ Lab - Guard.playground/Pages/1. Exercise - Guard Statements.xcplaygroundpage/Contents.swift
@@ -4,13 +4,22 @@ import UIKit
 
  Imagine you want to write a function to calculate the area of a rectangle. However, if you pass a negative number into the function, you don't want it to calculate a negative area. Create a function called `calculateArea` that takes two `Double` parameters, `x` and `y`, and returns an optional `Double`. Write a guard statement at the beginning of the function that verifies each of the parameters is greater than zero and returns `nil` if not. When the guard has succeeded, calculate the area by multiplying `x` and `y` together, then return the area. Call the function once with positive numbers and once with at least one negative number.
 */
+func calculateArea (x: Double, y: Double)-> Double? {
+    guard x > 0 && y > 0 else { return nil  }
+    return x*y
+}
 
-
+calculateArea(x: 34, y: 12.5)
+calculateArea(x: -12, y: 100)
 /*:
  Create a function called `add` that takes two optional integers as parameters and returns an optional integer. You should use one `guard` statement to unwrap both optional parameters, returning `nil` in the `guard` body if one or both of the parameters doesn't have a value. If both parameters can successfully be unwrapped, return their sum. Call the function once with non-`nil` numbers and once with at least one parameter being `nil`.
  */
-
-
+func add(i: Int?, j: Int?)-> Int? {
+    guard let ii = i, let jj = j else { return nil }
+    return ii + jj
+}
+add(i: 4, j: 5)
+add(i: nil, j: 5)
 /*:
  When working with UIKit objects, you will occasionally need to unwrap optionals to handle user input. For example, the text fields initialized below have `text` properties that are of type `String?`. 
  
@@ -30,10 +39,17 @@ firstNameTextField.text = "Jonathan"
 lastNameTextField.text = "Sanders"
 ageTextField.text = "28"
 
-
+func  createUser ()-> User? {
+    guard  let firstName = firstNameTextField.text, let lastName = lastNameTextField.text, let age = ageTextField.text else { return nil }
+    return User(firstName: firstName, lastName: lastName, age: age)
+    
+}
 /*:
  Call the function you made above and capture the return value. Unwrap the `User` with standard optional binding and print a statement using each of its properties. 
  */
-
+let User1 = createUser()
+if let user = User1 {
+    print("L'utente si chiama \(user.firstName)  \(user.lastName) e ha \(user.age) anni")
+}
 
 //: page 1 of 2  |  [Next: App Exercise - Guard](@next)
