1c8bfb54a8c6978b5d8f588d5a3db51928e1d2bc
Matt Hanlon <mhanlon@thecodehub.ie>
2021-02-08 14:05:22 +0000

    Finishe th first ab.

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Hello, End user prep!")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
