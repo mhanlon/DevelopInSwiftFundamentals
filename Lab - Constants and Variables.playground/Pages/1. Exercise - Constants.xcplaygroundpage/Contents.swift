1cbfec2d52d412eb2389e795643a23e5b33d8a89
Matt Hanlon <mhanlon@thecodehub.ie>
2021-04-20 13:41:08 +0100

    Finished the constants playground.

--- Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -3,8 +3,9 @@
 
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
-
+let friends = 500
+print(friends)
+// Why use constants at all?
 /*:
  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
  */
