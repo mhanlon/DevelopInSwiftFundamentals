cafd4f66178891e5ad29fc9807ec2901611cdd75
tmaskall <tmaskall@eastleigh.ac.uk>
2021-05-22 09:19:32 +0100

    Update Contents.swift

--- Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -12,7 +12,7 @@ print("friends") // output to the terminal
  */
 // friends = friends -1
 // cant do the above its a constant
-
+// added comment to test git
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
  */
