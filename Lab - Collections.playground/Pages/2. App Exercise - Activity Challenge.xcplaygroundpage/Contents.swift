599282ae2d55443058a81fe746bb8f5b58c4e12e
samuel <samuel@flatpie-II.local>
2021-06-20 14:45:15 +0200

    Done collections

--- Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -7,26 +7,36 @@
  
  Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
  */
-
+let walkingChallenges = ["Walk 3 miles a day", "Walk 30 miles a week"]
+let runningChallanges = ["Run 5 times a week","Run 6 miles"]
 
 /*:
  In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
  */
-
+var challenges = [walkingChallenges, runningChallanges]
+print(challenges[1][0])
 
 /*:
  All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
  */
-
+challenges.removeAll()
+print(challenges)
 
 /*:
  Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
  */
-
+var userChallenges = [String]()
 
 /*:
  Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
  */
-
-
+userChallenges.append("Sample challenge 1")
+userChallenges.append("Sample challenge 2")
+if userChallenges.isEmpty {
+    print("Please selected a challenge!")
+}else if userChallenges.count == 1 {
+    print("The challenge you have chosen is: \(userChallenges[0])")
+} else {
+    print("You have chosen multiple challenges.")
+}
 //: [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Dictionaries](@next)
--- Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -3,33 +3,36 @@
 
  Create a variable `[String: Int]` dictionary that can be used to look up the number of days in a particular month. Use a dictionary literal to initialize it with January, February, and March. January contains 31 days, February has 28, and March has 31. Print the dictionary.
  */
-
-
+var dpm = ["January": 31, "February": 28, "March": 31]
+print(dpm)
 /*:
  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
  */
-
-
+dpm["April"] = 30
+print(dpm)
 /*:
  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
  */
-
-
+dpm.updateValue(29, forKey: "February")
+print(dpm)
 /*:
  Use if-let syntax to retrieve the number of days under "January". If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
  */
-
-
+if let jdays = dpm["January"] {
+    print("January has \(jdays) days")
+}
 /*:
  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors". Print the resulting dictionary.
  */
 let shapesArray = ["Circle", "Square", "Triangle"]
 let colorsArray = ["Red", "Green", "Blue"]
 
-
+var shapeColors = ["Shapes": shapesArray, "Colors": colorsArray]
+print(shapeColors)
 /*:
  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
  */
-
-
+if let colors = shapeColors["Colors"] {
+    print(colors.last!)
+}
 //: [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
