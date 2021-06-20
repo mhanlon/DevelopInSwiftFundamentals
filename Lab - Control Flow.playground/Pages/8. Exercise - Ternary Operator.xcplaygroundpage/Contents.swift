51e86b88e130a92d5a48743370a55142ab4000d8
samuel <samuel@flatpie-II.local>
2021-06-20 14:43:43 +0200

    Done control flow

--- Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++ Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -6,11 +6,5 @@
 let number1 = 14
 let number2 = 25
 
-var largest: Int
-if number1 > number2 {
-    largest = number1
-} else {
-    largest = number2
-}
-
+var largest = number1 > number2 ? number1 : number2
 //: [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
