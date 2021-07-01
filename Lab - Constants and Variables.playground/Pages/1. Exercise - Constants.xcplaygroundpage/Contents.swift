0dca3a2f8c6b8f1fb83ed0dc18f185ecd52e2324
Matt Hanlon <mhanlon@thecodehub.ie>
2021-07-01 14:36:02 +0100

    Finished all the labs.

--- Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -3,8 +3,8 @@
 
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
-
+let friends = 100
+print(friends)
 /*:
  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
  */
