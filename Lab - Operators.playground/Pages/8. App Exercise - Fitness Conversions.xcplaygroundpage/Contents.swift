fecc355cd2707f83b815b17bf546a1b05b51b9b8
Aresar <macuser@Aresar.local>
2021-07-24 15:40:27 +0100

    finishing Operators

--- Lab - Operators.playground/Pages/8. App Exercise - Fitness Conversions.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/8. App Exercise - Fitness Conversions.xcplaygroundpage/Contents.swift
@@ -7,8 +7,8 @@
 
  Now create a constant `percentOfGoal` of type `Double` that equals the percent of the goal that has been reached so far. You'll need to convert your constants of type `Int` to be of type `Double` in your calculation.
  */
-
-
+var steps: Int = 7_000
+let percentOfGoal : Double = (Double(steps) / 10_000) * 100
 /*:
 
  _Copyright © 2018 Apple Inc._
