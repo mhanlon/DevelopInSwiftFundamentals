bf88f0a9a66c095e4ae34a66f36aff3418e03558
hbenlarh <hajarbenlarhzal@gmail.com>
2021-07-26 18:27:41 +0100

    finishing control flow

--- Lab - Control Flow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
+++ Lab - Control Flow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
@@ -16,6 +16,19 @@
  If `currentHR` is above the listed zones, print some kind of warning asking the user to slow down.
  */
 let currentHR = 128
-
+switch currentHR {
+case 100...120:
+    print("You are in the Very Light zone. Activity in this zone helps with recovery.")
+case 121...140:
+    print("You are in the Light zone. Activity in this zone helps improve basice endurance and fat burning.")
+case 141...160:
+    print("You are in the Moderate zone. Activity in this zone helps improve aerobic fitness.")
+case 161...180:
+    print("You are in the Hard zone. Activity in this zone increases maximum performance capacity for shorter sessions.")
+case 181...200:
+    print("You are in the Maximum zone. Activity in this zone helps fit athletes develop speed.")
+default:
+    print("woaah slow down ")
+}
 
 //: [Previous](@previous)  |  page 7 of 9  |  [Next: Exercise - Ternary Operator](@next)
--- Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
+++ Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
@@ -8,13 +8,13 @@
 let stepGoal = 10000
 let steps = 3948
 
-if steps < stepGoal / 2 {
-    print("Almost halfway!")
-} else {
-    print("Over halfway!")
-}
+//if steps < stepGoal / 2 {
+//    print("Almost halfway!")
+//} else {
+//    print("Over halfway!")
+//}
 
-
+steps < stepGoal / 2 ? print("Almost halfway!") : print("Over halfway!")
 /*:
 
  _Copyright © 2018 Apple Inc._
